7dd8a9fb90eb7d0b14d009ce97b45e6de199bb7f
douglassmoss <douglassmoss@icloud.com>
2021-12-15 09:20:18 -0800

    completed

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+
+// GLOBAL VARIABLE
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+const drivers2 = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+  function findMatching(drivers, string) {
+    return drivers.filter(function(driverName) {
+        return driverName.toLowerCase() === string.toLowerCase()
+    });
+};
+function fuzzyMatch(drivers, string) {
+let lengthOfName = string.length
+    return drivers.filter(function(element, index, array) {
+        return element.slice(0, lengthOfName) === string;
+    });
+};
+function matchName(drivers2, string) {
+    return drivers2.filter(function(element, index, array) {
+        return element.name.toLowerCase() === string.toLowerCase();
+    });
+};
